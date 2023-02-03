e74b7daf7e0d632716477c3d38011598702219f5
Jose Farias <fariasjoss@gmail.com>
2023-02-02 19:08:55 -0500

    Desafio encriptador

---
+++ script.js
@@ -0,0 +1,33 @@
+function encriptar (){
+        var texto = document.querySelector("#input-texto").value;
+        var textoCifrado = texto.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
+        document.querySelector(".text-input-salida").value = textoCifrado;
+        document.querySelector("#input-texto").value = "";
+        document.querySelector(".text-input-salida").style.backgroundImage = "none";
+        
+        
+
+}
+
+var boton1 = document.querySelector("#btn-encriptar");
+boton1.onclick = encriptar;
+
+function desencriptar (){
+        var texto = document.querySelector("#input-texto").value;
+        var textoCifrado = texto.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u");
+        document.querySelector(".text-input-salida").value = textoCifrado;
+        document.querySelector("#input-texto").value;
+
+}
+
+var boton2 = document.querySelector("#btn-desencriptar");
+boton2.onclick = desencriptar;
+
+var boton3 = document.querySelector("#btn-copy");
+boton3.onclick = copiar;
+
+function copiar() {
+  var copyText = document.querySelector(".text-input-salida");
+  navigator.clipboard.writeText(copyText.value);
+
+}
